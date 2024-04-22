90c345e604e8c8abbbc026464bd91413885a7277
JOSU9435 <kamalnetra702@gmail.com>
2024-04-22 20:48:37 +0530

    add ui

--- packages/client/src/pages/Home/Home.jsx
+++ packages/client/src/pages/Home/Home.jsx
@@ -1,7 +1,73 @@
+import { useState } from "react";
 import styles from "./Home.module.scss";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
 
 const Home = () => {
-  return <div className={styles.home}>Home</div>;
+  const [stock, setStock] = useState("");
+
+  const handleChange = (event) => {
+    setStock(event.target.value);
+  };
+
+  return (
+    <div className={styles.home}>
+      <div className={styles.controls}>
+        <FormControl size="small" margin="normal" color="secondary">
+          <InputLabel>Stock</InputLabel>
+          <Select value={stock} label="Age" onChange={handleChange}>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button size="medium" variant="contained" color="secondary">
+          Submit
+        </Button>
+      </div>
+      <div className={styles.transactions}>
+        <Card sx={{ margin: "1rem", backgroundColor: "#E8EEF7" }}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              title
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              description
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ margin: "1rem" }}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              title
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              description
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ margin: "1rem" }}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              title
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              description
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
